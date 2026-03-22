8bff45b3c7177da0f4d47a4112c36525891a8445
Velfraz <termi3ser@gmail.com>
2026-03-22 17:38:25 +0200

    CHT-2 Реалізовано розрахунок звітності

--- main.cpp
+++ main.cpp
@@ -16,7 +16,27 @@ int main() {
         expenses.push_back(amount);
         cout << "Added: " << amount << ". Next: ";
     }
+    
+    cout << "\n----------------------------" << endl;
+    if (expenses.empty()) {
+        cout << "No expenses to calculate." << endl;
+    }
+    else {
+        double total = 0;
+        for (double e : expenses) {
+            total += e;
+        }
 
+        double average = total / expenses.size();
+
+        cout << "Total Expenses: " << total << " UAH" << endl;
+        cout << "Number of items: " << expenses.size() << endl;
+        cout << "Average Spend: " << average << " UAH" << endl;
+    }
+    cout << "----------------------------" << endl;
+    cout << "Press Enter to exit...";
+    cin.ignore();
+    cin.get();
     
 
     return 0;
